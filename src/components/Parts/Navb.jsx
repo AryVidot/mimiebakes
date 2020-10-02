32929c9e5fb9d50a5e2c41bf6228c74c8bc40e4d
Ary Vidot <aryvid@gmail.com>
2020-10-02 13:58:22 +0100

    added as={Link} to the nav.Link lines - This should work once deployed

--- src/components/Parts/Navb.jsx
+++ src/components/Parts/Navb.jsx
@@ -2,31 +2,53 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "./navbar.scss";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
+
+// import Home from "../Pages/Home";
+// import About from "../Pages/About";
+// import Pricing from "../Pages/Pricing";
+// import Contact from "../Pages/Contact";
 
 function Navb() {
   return (
     <section>
-      <Navbar
-        className="menu-toggle menu-b "
-        collapseOnSelect
-        expand="lg"
-        bg="white"
-        variant="light"
-        fixed="top"
-      >
-        <Navbar.Brand href="#home">Mimie Bakes.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav justify-content-right">
-          <Nav className="mr-auto  ">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/pricing">Pricing</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+      <div>
+        <Navbar
+          className="menu-toggle menu-b "
+          collapseOnSelect
+          expand="lg"
+          bg="light"
+          variant="light"
+          fixed="top"
+        >
+          <Navbar.Brand href="#home">Mimie Bakes.</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav justify-content-right">
+            <Nav className="mr-auto  ">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/pricing">
+                Pricing
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      {/* <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div> */}
       <hr></hr>
     </section>
   );
